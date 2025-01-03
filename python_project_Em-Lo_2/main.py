b9f64b53cee9dbcab9a3a896a7966d6ad58ca772
LolaLT <lola.lievain-thiebaut@efrei.net>
2025-01-03 22:15:54 +0100

    Update main.py
    just added the logic challenge import

--- python_project_Em-Lo_2/main.py
+++ python_project_Em-Lo_2/main.py
@@ -1,7 +1,10 @@
 from math_challenges import *
 from pere_fouras_challenge import *
+from logic_challenge import *
+
 
 # main programme pour les challenges de math
 if __name__ == '__main__' :
     riddles = load_riddles('PFRiddles.json')
     print(riddles)
+    
